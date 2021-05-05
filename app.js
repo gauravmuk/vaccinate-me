7e23c635235bfa7379c2b456b0291dea3d3b0398
Gaurav Nanda <gaurav.nanda@wingify.com>
2021-05-05 23:04:32 +0530

    Access from India

--- app.js
+++ app.js
@@ -1,25 +1,124 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  let html = "";
-  try {
-    let data = await fetch(`.netlify/functions/vaccinate`);
-    data = await data.json();
+function getDate(dateDiff) {
+  let date = new Date().getDate() + dateDiff;
+  let month = new Date().getMonth() + 1;
+  let year = new Date().getFullYear();
 
-    if (!(data && data.length)) {
-      document.querySelector("#alert-box-happy").classList.remove("d-none");
-      return;
+  date = date < 10 ? `0${date}` : date;
+  month = month < 10 ? `0${month}` : month;
+
+  return `${date}-${month}-${year}`;
+}
+
+async function getXHRData(url) {
+  try {
+    let data = await fetch(url, {
+      headers: {
+        accept: "application/json, text/plain, */*",
+        "accept-language": "en",
+        "Access-Control-Allow-Headers": "GET",
+        "cache-control": "no-cache",
+        pragma: "no-cache",
+        "sec-ch-ua":
+          '" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "cross-site",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent":
+          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
+      },
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: null,
+      method: "GET",
+      mode: "cors",
+      "credentials": "omit"
+    });
+
+    console.log(url, data.status);
+
+    if (data.status === 200) {
+      data = await data.json();
+      return data;
+    } else {
+      return false;
+    }
+  } catch (e) {
+    return false;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const finalCenters = [];
+    const finalDates = [];
+    const days = 7;
+
+    for (let i = 1; i <= days; i++) {
+      finalDates.push(getDate(i));
     }
 
-    for (let i = 0; i < data.length; i++) {
-      html += `<tr>
+    let { districts } = await getXHRData(
+      "https://cdn-api.co-vin.in/api/v2/admin/location/districts/9"
+    );
+
+    if (!districts) {
+      return false;
+    }
+
+    for (let i = 0; i < finalDates.length; i++) {
+      for (let j = 0; j < districts.length; j++) {
+        let slots = await getXHRData(
+          `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=${districts[j].district_id}&date=${finalDates[i]}`
+        );
+
+        if (slots && slots.centers && slots.centers.length) {
+          const centers = slots.centers.filter(
+            (center) =>
+              center &&
+              center.fee_type &&
+              center.fee_type.toLowerCase() === "free"
+          );
+
+          for (let i = 0; i < centers.length; i++) {
+            if (centers[i].sessions && centers[i].sessions.length) {
+              const sessions = centers[i].sessions.filter(
+                (session) =>
+                  session.vaccine.toUpperCase() === "COVISHIELD" &&
+                  session.min_age_limit === 18 &&
+                  session.available_capacity >= 3
+              );
+
+              if (sessions.length) {
+                finalCenters.push({
+                  name: centers[i].name,
+                  address: centers[i].address,
+                  pincode: centers[i].pincode,
+                });
+              }
+            }
+          }
+        }
+      }
+    }
+
+    let html = "";
+    try {
+      for (let i = 0; i < finalCenters.length; i++) {
+        html += `<tr>
       <th scope="row">${i + 1}</th>
       <td>${data[i].name}</td>
       <td>${data[i].pincode}</td>
       <td>${data[i].address}</td>
     </tr>`;
-    }
+      }
 
-    document.querySelector("tbody").innerHTML = html;
-  } catch (e) {
-    document.querySelector("#alert-box-sad").classList.remove("d-none");
+      document.querySelector("tbody").innerHTML = html;
+    } catch (e) {
+      document.querySelector("#alert-box-sad").classList.remove("d-none");
+    }
+  } catch (err) {
+    console.log(err);
   }
 });
