8e06918bcdadec60f5dfa7faffc1bcbca270df11
Gaurav Nanda <gaurav.nanda@wingify.com>
2021-05-05 22:36:18 +0530

    Final Build

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  let data = await fetch(`.netlify/functions/vaccinate`);
-  data = data.json();
-  console.log(data);
+  let html = "";
+  try {
+    let data = await fetch(`.netlify/functions/vaccinate`);
+    data = await data.json();
+
+    if (!(data && data.length)) {
+      document.querySelector("#alert-box-happy").classList.remove("d-none");
+      return;
+    }
+
+    for (let i = 0; i < data.length; i++) {
+      html += `<tr>
+      <th scope="row">${i + 1}</th>
+      <td>${data[i].name}</td>
+      <td>${data[i].pincode}</td>
+      <td>${data[i].address}</td>
+    </tr>`;
+    }
+
+    document.querySelector("tbody").innerHTML = html;
+  } catch (e) {
+    document.querySelector("#alert-box-sad").classList.remove("d-none");
+  }
 });
